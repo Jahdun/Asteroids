c8374bd3fba9cf80671da24d03842bfde45576ee
Jahdun <Jorthomise@yahoo.com>
2025-01-20 18:53:01 -0500

    Sprites

--- main.py
+++ main.py
@@ -8,13 +8,19 @@ print(f"Screen height: {SCREEN_HEIGHT}")
 def main():
     pygame.init()
     screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
+    clock = pygame.time.Clock()
+    dt = 0
 
     while True:
-        screen.fill((0, 0, 0))
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
                 return
+        
+        screen.fill("black")
         pygame.display.flip()
+
+
+        dt = clock.tick(60) / 1000 
     
     
 
